b4ba2a3e1e6ede988a28ecfa2eab86b4d140265c
rodolforani <80513543+rodolforani@users.noreply.github.com>
2021-03-31 18:26:16 +0200

    Create R_code_time_series.r

---
+++ R_code_time_series.r
@@ -0,0 +1,11 @@
+Time series analysis of Greenland increase temperature
+Data and code from Emanuela Cosma
+
+#installiamo e attiviamo i pacchetti che ci servono (raster e rasterVis):
+#install.packages("raster")
+install.packages("rasterVis")
+library(raster)
+library(rasterVis)
+
+#settiamo la cartella di lavoro:
+setwed(C:/lab/Greenland)
