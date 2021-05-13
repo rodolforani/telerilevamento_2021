fb813e209d93a46e940d033c95d77e6a3090839a
rodolforani <80513543+rodolforani@users.noreply.github.com>
2021-05-13 19:12:08 +0200

    Update exame.r

--- exame.r
+++ exame.r
@@ -1,1 +1,14 @@
 Inizio codice per esame
+
+install.packages("ncdt4")
+library(raster)
+library(rasterdiv)
+library(rasterVis)
+library(ncdf4)
+
+setwd("c:/lab/Esame")
+
+neve12_05_2021 <- raster("c_gls_SCE500_202105120000_CEURO_MODIS_V1.0.1.nc")
+neve12_05_2021
+neve12_05_2021ok <- reclassify(neve12_05_2021, cbind(0:20,NA))
+plot(neve12_05_2021)
