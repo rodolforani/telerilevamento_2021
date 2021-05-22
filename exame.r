d16956c9cec181e5acd17f8fe47c87852503bf8b
rodolforani <80513543+rodolforani@users.noreply.github.com>
2021-05-22 17:51:01 +0200

    Update exame.r

--- exame.r
+++ exame.r
@@ -1,14 +1,28 @@
-Inizio codice per esame
+# Prova esame variabilità calanchi--->> IMPOSSIBILE DISTINGUERE CON IMMAGINI COSì ESTESE, QUIDNI ALTRO OGGETTO DI ANALISI??
 
-install.packages("ncdt4")
 library(raster)
-library(rasterdiv)
 library(rasterVis)
-library(ncdf4)
+library(RStoolbox)
+library(ggplot2) 
+library(gridExtra)
+library(viridis)
 
-setwd("c:/lab/Esame")
+setwd("C:/lab/Esame")
 
-neve12_05_2021 <- raster("c_gls_SCE500_202105120000_CEURO_MODIS_V1.0.1.nc")
-neve12_05_2021
-neve12_05_2021ok <- reclassify(neve12_05_2021, cbind(0:20,NA))
-plot(neve12_05_2021)
+# creazione di una lista di file che ha qualcosa in comune nel nome (pattern)
+# il pattern è il percorso di riconoscimento dei file ossia il fatto che hanno un pezzo di nome comune "lst"
+# associo la lista al nome rlist
+list10m2019 <- list.files(pattern="T32TPQ_20190228T101029")
+list10m2019
+# ora applico la funzione raster alla lista con la funzione lapply
+# lapplay necessita della lista di dati su cui vogliamo applicare la funzione, che scriviamo dopo la virgola (listadati,funzione) 
+# nomino le immmagini insieme con import
+import10m2019 <- lapply(list10m2019,raster)
+import10m2019
+# adesso abbiamo i singoli file importati e per facilitare le future operazioni 
+# unisco le immagini in un unico file come se fossero più livelli (bande) all'interno di un unico file
+# salvo tutto con un nome file TGr
+R10m2019 <- stack(import10m2019)
+R10m2019
+plot(R10m2019$T32TPQ_20190228T101029_B02_10m)
+plotRGB(R10m2019, 1,2,3, stretch="Lin")
